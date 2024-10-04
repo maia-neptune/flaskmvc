b52691bdc8939654148bc8c22891a2af0c9fefcc
jonsnation <jonathanswamber2003@gmail.com>
2024-10-03 21:02:35 -0400

    test code for user uncommented

--- wsgi.py
+++ wsgi.py
@@ -248,21 +248,21 @@ def remove_tutor(id):
 
 # app.cli.add_command(user_cli) # add the group to the cli
 
-# '''
-# Test Commands
-# '''
+'''
+Test Commands
+'''
 
-# test = AppGroup('test', help='Testing commands') 
+test = AppGroup('test', help='Testing commands') 
 
-# @test.command("user", help="Run User tests")
-# @click.argument("type", default="all")
-# def user_tests_command(type):
-#     if type == "unit":
-#         sys.exit(pytest.main(["-k", "UserUnitTests"]))
-#     elif type == "int":
-#         sys.exit(pytest.main(["-k", "UserIntegrationTests"]))
-#     else:
-#         sys.exit(pytest.main(["-k", "App"]))
+@test.command("user", help="Run User tests")
+@click.argument("type", default="all")
+def user_tests_command(type):
+    if type == "unit":
+        sys.exit(pytest.main(["-k", "UserUnitTests"]))
+    elif type == "int":
+        sys.exit(pytest.main(["-k", "UserIntegrationTests"]))
+    else:
+        sys.exit(pytest.main(["-k", "App"]))
     
 
-# app.cli.add_command(test)
+app.cli.add_command(test)
